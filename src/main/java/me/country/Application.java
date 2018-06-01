a29e52726940cf56dfb765441d259c1d8d8ea189
country <hnaras@naver.com>
2018-06-01 22:14:22 +0900

    Day 10. @ConfigurationProperties의 여러 장점과 유일한 단점

--- src/main/java/me/country/Application.java
+++ src/main/java/me/country/Application.java
@@ -1,15 +1,17 @@
 package me.country;
 
+import me.country.hello.CountryProperties;
 import me.country.hello.HelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.Banner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Bean;
 import org.springframework.core.env.Environment;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @SpringBootApplication
 @RestController
@@ -21,10 +23,16 @@ public class Application {
     @Autowired
     Environment environment;
 
+    @Bean
+    @ConfigurationProperties("country")
+    @Validated
+    public CountryProperties countryProperties() {
+        return new CountryProperties();
+    }
+
     @RequestMapping("/")
     public String hello() {
-        System.out.println(environment.getProperty("country.list[0].descr", List.class));
-        System.out.println(environment.getProperty("country.name"));
+        System.out.println(countryProperties().getNumber2());
         return helloService.getMessage();
     }
 
